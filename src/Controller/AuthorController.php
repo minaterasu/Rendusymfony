1207e146978bf919f6fa8d17f1c1634ee0ef9bd4
minaterasu <benghorbel.nadhir@esprit.tn>
2023-10-01 12:24:09 +0100

    l rendu final

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,97 @@
+<?php
+
+
+// AuthorController.php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+// AuthorController.php
+
+// ...
+
+class AuthorController extends AbstractController
+{
+    // ...
+
+    #[Route('/author/list', name: 'author_list')]
+    public function list(): Response
+    {
+        $authors = [
+            [
+                'id' => 1,
+                'picture' => '/Images/VictorHugo.jpg',
+                'username' => 'Victor Hugo',
+                'email' => 'victor.hugo@gmail.com',
+                'nb_books' => 100
+            ],
+            [
+                'id' => 2,
+                'picture' => '/Images/williamshakespeare.jpg',
+                'username' => 'William Shakespeare',
+                'email' => 'william.shakespeare@gmail.com',
+                'nb_books' => 200
+            ],
+            [
+                'id' => 3,
+                'picture' => '/Images/TahaHussein.jpg',
+                'username' => 'Taha Hussein',
+                'email' => 'taha.hussein@gmail.com',
+                'nb_books' => 300
+            ],
+        ];
+
+        return $this->render('author/list.html.twig', [
+            'authors' => $authors,
+        ]);
+    }
+
+    #[Route('/author/details/{id}', name: 'author_details')]
+    public function authorDetails($id): Response
+    {
+        $authors = [
+            [
+                'id' => 1,
+                'picture' => '/Images/VictorHugo.jpg',
+                'username' => 'Victor Hugo',
+                'email' => 'victor.hugo@gmail.com',
+                'nb_books' => 100
+            ],
+            [
+                'id' => 2,
+                'picture' => '/Images/williamshakespeare.jpg',
+                'username' => 'William Shakespeare',
+                'email' => 'william.shakespeare@gmail.com',
+                'nb_books' => 200
+            ],
+            [
+                'id' => 3,
+                'picture' => '/Images/TahaHussein.jpg',
+                'username' => 'Taha Hussein',
+                'email' => 'taha.hussein@gmail.com',
+                'nb_books' => 300
+            ],
+        ];
+
+        $author = null;
+        foreach ($authors as $a) {
+            if ($a['id'] == $id) {
+                $author = $a;
+                break;
+            }
+        }
+
+        if (!$author) {
+            throw $this->createNotFoundException('Author not found');
+        }
+
+        return $this->render('author/showAuthor.html.twig', [
+            'author' => $author,
+        ]);
+    }
+}
+?>
---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ServiceController extends AbstractController
+{
+    #[Route('/service/{name}', name: 'app_service')]
+    public function showService($name): Response
+    {
+        return $this->render('service/index.html.twig', [
+            'name' => $name,
+        ]);
+    }
+
+    #[Route('/go-to-index', name: 'go_to_index')]
+    public function goToIndex(): Response
+    {
+        
+        return $this->redirectToRoute('app_home');
+    }
+
+   
+}
